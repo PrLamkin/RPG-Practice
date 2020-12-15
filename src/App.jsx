b9753459eda981136433f069f2ad5aa7cd4e1963
PrLamkin <shiektheimmortal@outlook.com>
2020-12-14 22:24:36 -0600

    slowly making adjustments to this code

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,14 @@ import Sprite from "./components/sprite"
 export default function App() {
   return (
   <div className="zone-container">
-    <Sprite />
+    <Sprite image={"/sprites/skins/m1.png"}
+      data={{
+        x: 0,
+        y: 0,
+        w: 32,
+        h: 32,
+      }}
+    />
   </div>
   );
 }
